baa691811f53a809770f2c4009bd5bf4c7b93e46
deprilula28 <deprilula28@gmail.com>
2018-08-13 15:20:21 -0300

    updoot

--- src/main/java/me/deprilula28/gamesrob/utility/Cache.java
+++ src/main/java/me/deprilula28/gamesrob/utility/Cache.java
@@ -35,13 +35,8 @@ public class Cache {
             while (true) {
                 try {
                     Random random = ThreadLocalRandom.current();
-                    long begin = System.nanoTime();
 
-                    double thing = (45 + (Math.sqrt(random.nextInt(120 - 90) + 90 + ((System.currentTimeMillis() * 0.0000000001)
-                                    + (Math.sqrt(Math.pow(random.nextDouble() * (256 - 248) + 248, 2)) -
-                                    random.nextDouble() * (4.20 - 0.69) + 0.69)))));
-
-                    Thread.sleep(Math.round(thing * 1000));
+                    Thread.sleep(90);
                     long time = System.currentTimeMillis();
                     int removed = 0;
 
